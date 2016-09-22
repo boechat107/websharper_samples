6687fcdb65340b5cd1f65919074cd72f97841d36
Andre Ambrosio Boechat <boechat107@gmail.com>
2016-09-22 11:40:35 -0300

    First commit; self-hosted with Owin

---
+++ wsnextui/WsNextui/Main.fs
@@ -0,0 +1,86 @@
+namespace WsNextui
+
+open WebSharper
+open WebSharper.Sitelets
+open WebSharper.UI.Next
+open WebSharper.UI.Next.Server
+
+type EndPoint =
+    | [<EndPoint "/">] Home
+    | [<EndPoint "/about">] About
+
+module Templating =
+    open WebSharper.UI.Next.Html
+
+    type MainTemplate = Templating.Template<"Main.html">
+
+    // Compute a menubar where the menu item for the given endpoint is active
+    let MenuBar (ctx: Context<EndPoint>) endpoint : Doc list =
+        let ( => ) txt act =
+             liAttr [if endpoint = act then yield attr.``class`` "active"] [
+                aAttr [attr.href (ctx.Link act)] [text txt]
+             ]
+        [
+            li ["Home" => EndPoint.Home]
+            li ["About" => EndPoint.About]
+        ]
+
+    let Main ctx action title body =
+        Content.Page(
+            MainTemplate.Doc(
+                title = title,
+                menubar = MenuBar ctx action,
+                body = body
+            )
+        )
+
+module Site =
+    open WebSharper.UI.Next.Html
+
+    let HomePage ctx =
+        Templating.Main ctx EndPoint.Home "Home" [
+            h1 [text "Say Hi to the server!"]
+            div [client <@ Client.Main() @>]
+        ]
+
+    let AboutPage ctx =
+        Templating.Main ctx EndPoint.About "About" [
+            h1 [text "About"]
+            p [text "This is a template WebSharper client-server application."]
+        ]
+
+    [<Website>]
+    let Main =
+        Application.MultiPage (fun ctx endpoint ->
+            match endpoint with
+            | EndPoint.Home -> HomePage ctx
+            | EndPoint.About -> AboutPage ctx
+        )
+
+module SelfHostedServer =
+
+    open global.Owin
+    open Microsoft.Owin.Hosting
+    open Microsoft.Owin.StaticFiles
+    open Microsoft.Owin.FileSystems
+    open WebSharper.Owin
+
+    [<EntryPoint>]
+    let Main args =
+        let rootDirectory, url =
+            match args with
+            | [| rootDirectory; url |] -> rootDirectory, url
+            | [| url |] -> "..", url
+            | [| |] -> "..", "http://localhost:9000/"
+            | _ -> eprintfn "Usage: WsSimple ROOT_DIRECTORY URL"; exit 1
+       // stdout.WriteLine("my print: {0} -- {1}", rootDirectory, url)
+       // 0
+        use server = WebApp.Start(url, fun appB ->
+            appB.UseStaticFiles(
+                    StaticFileOptions(
+                        FileSystem = PhysicalFileSystem(rootDirectory)))
+                .UseSitelet(rootDirectory, Site.Main)
+            |> ignore)
+        stdout.WriteLine("Serving {0}", url)
+        stdin.ReadLine() |> ignore
+        0
